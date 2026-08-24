d0b188224be90c036ec16159a94b115f4df9a8ae
root <root@ip-172-31-19-86.ap-south-1.compute.internal>
2026-08-24 16:27:20 +0000

    Update EKS cluster configuration for ap-south-1

--- variables.tf
+++ variables.tf
@@ -1,14 +1,15 @@
 variable "kubernetes_version" {
-  default     = 1.27
-  description = "kubernetes version"
+  default     = "1.33"
+  description = "Kubernetes version"
 }
 
 variable "vpc_cidr" {
   default     = "10.0.0.0/16"
-  description = "default CIDR range of the VPC"
+  description = "Default CIDR range of the VPC"
 }
+
 variable "aws_region" {
-  default = "us-west-1"
-  description = "aws region"
+  default     = "ap-south-1"
+  description = "AWS region"
 }
 
--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,15 @@
-
 provider "aws" {
   region = var.aws_region
 }
 
-data "aws_availability_zones" "available" {}
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 
 locals {
-  cluster_name = "abhi-eks-${random_string.suffix.result}"
+  azs = slice(data.aws_availability_zones.available.names, 0, 2)
+
+  cluster_name = "employee-eks-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
@@ -18,11 +21,21 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.7.0"
 
-  name                 = "abhi-eks-vpc"
-  cidr                 = var.vpc_cidr
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24"]
+  name = "employee-eks-vpc"
+  cidr = var.vpc_cidr
+
+  azs = local.azs
+
+  private_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  public_subnets = [
+    "10.0.4.0/24",
+    "10.0.5.0/24"
+  ]
+
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
