8105f547b2ddf5f4b855b88c6f5d9401ba4a5d1d
Jenkins <jenkins@ip-172-31-19-86.ap-south-1.compute.internal>
2026-08-26 19:25:22 +0000

    Use t3.small for EKS node group

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -20,15 +20,15 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type               = "AL2023_x86_64_STANDARD"
-    instance_types         = ["t3.medium"]
+    instance_types         = ["t3.small"]
     vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
   }
 
   eks_managed_node_groups = {
     node_group = {
-      min_size     = 2
-      max_size     = 6
-      desired_size = 2
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1  
     }
   }
 }
