b420feaf2b5ff8e462b51b9675fc9c0f24758618
root <root@ip-172-31-19-86.ap-south-1.compute.internal>
2026-08-27 18:22:43 +0000

    Add EKS node groups labels and addons

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,6 +1,6 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "20.8.4"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
 
   cluster_name    = local.cluster_name
   cluster_version = var.kubernetes_version
@@ -10,7 +10,24 @@ module "eks" {
 
   enable_irsa = true
 
-  # Allow Kubernetes API access from both inside and outside the VPC
+  # EKS Managed Add-ons
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+
+    kube-proxy = {
+      most_recent = true
+    }
+
+    vpc-cni = {
+      most_recent = true
+    }
+
+    aws-ebs-csi-driver = {
+      most_recent = true
+    }
+  }
   cluster_endpoint_public_access  = true
   cluster_endpoint_private_access = true
 
@@ -25,11 +42,53 @@ module "eks" {
   }
 
   eks_managed_node_groups = {
-    node_group = {
+
+    frontend = {
       min_size     = 1
       max_size     = 2
-      desired_size = 1  
+      desired_size = 1
+
+      instance_types = ["t3.small"]
+
+      labels = {
+        workload = "frontend"
+      }
+    }
+
+    backend = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t3.small"]
+
+      labels = {
+        workload = "backend"
+      }
+    }
+
+    database = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t3.small"]
+
+      labels = {
+        workload = "database"
+      }
+    }
+
+    monitoring = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t3.small"]
+
+      labels = {
+        workload = "monitoring"
+      }
     }
   }
 }
-
